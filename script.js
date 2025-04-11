f38287cadda0e84951d8a072acd9419de06b1b25
lesronn <victoroforiacheampong@gmail.com>
2025-04-11 18:15:45 +0000

    Made the app responsive,  removed some unused styles, added interractivity with the models

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-
 function onReady(callback) {
-    var intervalId = window.setInterval(function() {
-      if (document.getElementsByTagName('body')[0] !== undefined) {
-        window.clearInterval(intervalId);
-        callback.call(this);
-      }
-    }, 1000);
-  }
-  
-  function setVisible(selector, visible) {
-    document.querySelector(selector).style.display = visible ? 'block' : 'none';
-  }
-  
-  onReady(function() {
-    setVisible('.page', true);
-    setVisible('#loading', false);
+  var intervalId = window.setInterval(function () {
+    if (document.getElementsByTagName("body")[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 300);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? "block" : "none";
+}
+
+window.addEventListener("load", function () {
+  // This fires when everything is fully loaded including all resources
+  console.log("Page fully loaded");
+  onReady(function () {
+    setVisible(".page", true);
+    setVisible("#loading", false);
   });
+});
 
-
-
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector(".progress-bar");
+  const updatingBar = event.target.querySelector(".update-bar");
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add("hide");
+    event.target.removeEventListener("progress", onProgress);
+  } else {
+    progressBar.classList.remove("hide");
+  }
+};
+document.querySelector("model-viewer").addEventListener("progress", onProgress);
